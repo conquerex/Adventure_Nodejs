87069403ebd4f02071618225d7c9216597aaaa73
Barley <barley@balancehero.com>
2018-04-16 14:08:53 +0900

    Handling file uploads

--- server.js
+++ server.js
@@ -6,19 +6,10 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData ="";
+    // var postData ="";
     var pathname = url.parse(request.url).pathname;
     console.log("Path name : "+pathname);
-
-    request.setEncoding("utf8");
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("Received POST data chunk : "+postDataChunk);
-    });
-
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData)
-    });
+    route(handle, pathname, response, request)
   }
   // http 모듈의 createServer 메소드를 호출하여 HTTP 서버 생성
   http.createServer(onRequest).listen(8888);
