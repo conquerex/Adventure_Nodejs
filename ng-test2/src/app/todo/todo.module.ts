c60c4128e3db96e5f8fadfeaf7e978b009739e0f
Barley <barley@balancehero.com>
2018-04-19 11:42:04 +0900

    Add a component

--- ng-test2/src/app/todo/todo.module.ts
+++ ng-test2/src/app/todo/todo.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todos/todo/todo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TodosComponent],
+  declarations: [TodosComponent, TodoComponent],
   exports: [TodosComponent]
 })
 export class TodoModule { }
