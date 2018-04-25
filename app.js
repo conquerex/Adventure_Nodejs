ba0958880df45242d055a7df0954de53c301cdeb
unknown <conquerex@gmail.com>
2018-04-25 23:41:19 +0900

    Read Static file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 app.get('/', function(req, res) { // router
   res.send('<h1>This is Home page.</h1>'); // controller
+});
+app.get('/route', function(req, res) {
+  res.send('Hello Router <br><img src="/nodejs-dark.jpg">');
 });
 app.get('/login', function(req, res) {
   res.send('Login please');
