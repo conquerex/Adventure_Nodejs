77e220f7f27dec113d992fd26f45ba8994075a54
unknown <conquerex@gmail.com>
2018-04-25 23:51:44 +0900

    Static and Dynamic expresion

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.get('/', function(req, res) { // router
 app.get('/route', function(req, res) {
   res.send('Hello Router <br><img src="/nodejs-dark.jpg">');
 });
+app.get('/dynamic', function(req, res) {
+  var lis = '';
+  var time = Date();
+  for (let i = 0; i < 5; i++) {
+    lis = lis + '<li>Coding</li>';
+  }
+  var output = `<!DOCTYPE html>
+  <html>
+  <head>
+      <meta charset="utf-8" />
+      <title>Page Title</title>
+  </head>
+  <body>
+      <h2>Hello Dynamic!!!!</h2>    
+      <ul>${lis}</ul>
+      ${time}
+  </body>
+  </html>`;
+  res.send(output);
+});
 app.get('/login', function(req, res) {
   res.send('Login please');
 });
