2722d6614f22f0a6c0f34bfe416f45a94721bfdb
unknown <conquerex@gmail.com>
2018-04-26 23:55:44 +0900

    Add new.pug

--- app_file.js
+++ app_file.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-app.set('views', './views_file');
-app.set('view engine', 'pug');
 app.locals.pretty = true;
 
+app.set('views', './views_file');
+app.set('view engine', 'pug');
+
 app.get('/topic/new', function(req, res){
-    res.send('Hello file~!!!!');
+    // res.send('Hello file~!!!!');
+    res.render('new');
 });
 app.listen(3000, function(){
     console.log('Connected, 3000 port!!!');
