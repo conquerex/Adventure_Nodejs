bd5570447d674ff892acc08776f1d8321e7a8519
손종국 <conquerex@gmail.com>
2018-04-27 16:33:56 +0900

    Modify topic get method

--- app_file.js
+++ app_file.js
@@ -15,36 +15,50 @@ app.get('/topic/new', function(req, res){
     res.render('new');
 });
 
-app.get('/topic', function(req, res) {
+app.get(['/topic', '/topic/:id'], function(req, res) {
     fs.readdir('data', function(err, files){
         if(err){
             console.log(err);
             res.status(500).send('Internal Server Error!!!'); 
         }
-        res.render('view', {topics:files});
-    });
-});
 
-app.get('/topic/:id', function(req, res){
-    var id = req.params.id;
-
-    fs.readdir('data', function(err, files){
-        if(err){
-            console.log(err);
-            res.status(500).send('Internal Server Error!!!'); 
+        var id = req.params.id; 
+        if(id) {
+            // id가 있는 경우
+            fs.readFile('data/'+id, 'utf8', function(err, data){
+                if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error!!!'); 
+                }
+                // res.send(data);
+                res.render('view', {topics:files, title:id, description:data});
+            });
+        } else {
+            // id가 없는 경우
+            res.render('view', {topics:files, title:'Welcome', description:'......'});
         }
-        fs.readFile('data/'+id, 'utf8', function(err, data){
-            if(err){
-                console.log(err);
-                res.status(500).send('Internal Server Error!!!'); 
-            }
-            // res.send(data);
-            res.render('view', {topics:files, title:id, description:data});
-        });
+        
     });
-
-    
 });
+
+// app.get('/topic/:id', function(req, res){
+//     var id = req.params.id;
+
+//     fs.readdir('data', function(err, files){
+//         if(err){
+//             console.log(err);
+//             res.status(500).send('Internal Server Error!!!'); 
+//         }
+//         fs.readFile('data/'+id, 'utf8', function(err, data){
+//             if(err){
+//                 console.log(err);
+//                 res.status(500).send('Internal Server Error!!!'); 
+//             }
+//             // res.send(data);
+//             res.render('view', {topics:files, title:id, description:data});
+//         });
+//     });
+// });
 
 app.post('/topic', function(req, res){
     var title = req.body.title;
